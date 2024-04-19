3ea2a75a5c7713daaa0911c7f4fbb99f5e55af52
sdepasqu <159298358+sdepasqu@users.noreply.github.com>
2024-04-19 11:00:03 -0500

    Update index.js

--- DNN_NSL-KDD/index.js
+++ DNN_NSL-KDD/index.js
@@ -1,9 +1,7 @@
-async function runExample() 
-{
+async function runExample() {
     var x = [];
 
-    for (let i = 1; i <= 41; i++) 
-    {
+    for (let i = 1; i <= 41; i++) {
         x.push(parseFloat(document.getElementById('box' + i).value));
     }
 
@@ -17,11 +15,20 @@ async function runExample()
 
     let predictions = document.getElementById('predictions');
 
-    predictions.innerHTML = ` <hr> Got an output tensor with values: <br/>
-    <table>
-    <tr>
-    <td>  Rating of Wine Quality  </td>
-    <td id="td0">  ${outputData.data[0].toFixed(2)}  </td>
-    </tr>
-    </table>`;
+    if (outputData) {
+        let data = outputData.data;
+        if (data && data.length > 0) {
+            predictions.innerHTML = ` <hr> Got an output tensor with values: <br/>
+                <table>
+                <tr>
+                <td>  Rating of Wine Quality  </td>
+                <td id="td0">  ${data[0].toFixed(2)}  </td>
+                </tr>
+                </table>`;
+        } else {
+            predictions.innerHTML = "Output data is empty.";
+        }
+    } else {
+        predictions.innerHTML = "Output data is undefined.";
+    }
 }
